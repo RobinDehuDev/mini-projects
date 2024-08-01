e3d48270dca1d8dceb369e53e288f41f9fbcb60e
RobinDehuDev <robin.dehu@gmail.com>
2024-08-01 14:48:09 +0400

    feat: first playable (client) version

--- src/app/game-of-life/Board.tsx
+++ src/app/game-of-life/Board.tsx
@@ -1,19 +1,63 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
+import { areBoardsIdentical, evaluateNewBoard } from "./game";
 
 export default function Board(props: { board: boolean[][] }) {
+  const [cycleNumber, setCycleNumber] = useState(0);
+  const [isRuning, setIsRuning] = useState(false);
   const [board, setBoard] = useState(props.board);
   const [previusBoard, setPreviusBoard] = useState(props.board);
   const [canUpdate, setUpdate] = useState(false);
 
+  function run() {
+    if (!isRuning) return undefined;
+
+    setCycleNumber(cycleNumber + 1);
+    const newboard = evaluateNewBoard(board);
+    const identical = areBoardsIdentical(board, newboard);
+    if (identical) {
+      setIsRuning(false);
+      return undefined;
+    }
+
+    setBoard(newboard);
+  }
+
+  useEffect(() => {
+    if (!canUpdate && isRuning) {
+      setTimeout(() => run(), 40);
+    }
+  }, [isRuning, canUpdate, board]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center gap-5 bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <section className="flex w-1/2 flex-col gap-2.5">
+    <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <section className="flex flex-col gap-10 pt-2">
+        {/* header */}
         <h1 className="self-center text-6xl font-bold">Jeu de la vie</h1>
-        <div className="bg- flex w-full gap-2.5">
-          <div className="ml-auto flex flex-col gap-2.5">
+        {/* controls */}
+        <section className="bg- flex w-full">
+          <div className="flex flex-1">Nombre de cycles : {cycleNumber}</div>
+          <div className="flex-1">
+            <button
+              disabled={canUpdate}
+              className={twMerge(
+                canUpdate ? "opacity-90" : "",
+                "w-24 rounded-lg bg-slate-300 px-2 py-0.5 font-medium text-slate-800",
+              )}
+              onMouseDown={() => {
+                if (isRuning) {
+                  setIsRuning(false);
+                } else {
+                  setIsRuning(true);
+                }
+              }}
+            >
+              {isRuning ? "Arrêter" : "Démarrer"}
+            </button>
+          </div>
+          <div className="flex flex-col gap-2.5">
             <button
               className="w-24 rounded-lg bg-slate-300 px-2 py-0.5 font-medium text-slate-800"
               onMouseDown={() => {
@@ -39,32 +83,33 @@ export default function Board(props: { board: boolean[][] }) {
               Annuler
             </button>
           </div>
-        </div>
-      </section>
-      <section className="flex flex-col gap-1">
-        {board.map((line, yPosition) => (
-          <div className="flex gap-1">
-            {line.map((cell, xPosition) => (
-              <Cell
-                value={cell}
-                canUpdate={canUpdate}
-                update={() => {
-                  console.log("click", yPosition, xPosition);
-                  const newBoard = [...board];
+        </section>
+        {/* board */}
+        <section className="flex flex-col gap-1">
+          {board.map((line, yPosition) => (
+            <div key={`line${yPosition}`} className="flex gap-1">
+              {line.map((cell, xPosition) => (
+                <Cell
+                  key={`cell${yPosition}-${xPosition}`}
+                  value={cell}
+                  canUpdate={canUpdate}
+                  update={() => {
+                    const newBoard = [...board];
 
-                  newBoard[yPosition] = board[yPosition]
-                    ? [...board[yPosition]]
-                    : [];
-                  newBoard[yPosition][xPosition] = !cell;
+                    newBoard[yPosition] = board[yPosition]
+                      ? [...board[yPosition]]
+                      : [];
+                    newBoard[yPosition][xPosition] = !cell;
 
-                  //   logBoard(newBoard);
+                    //   logBoard(newBoard);
 
-                  setBoard(newBoard);
-                }}
-              />
-            ))}
-          </div>
-        ))}
+                    setBoard(newBoard);
+                  }}
+                />
+              ))}
+            </div>
+          ))}
+        </section>
       </section>
     </main>
   );
@@ -82,10 +127,4 @@ function Cell(props: {
       onMouseDown={props.update}
     />
   );
-}
-
-function logBoard(board: boolean[][]) {
-  board.forEach((line) => {
-    console.log(line);
-  });
 }
--- src/app/game-of-life/page.tsx
+++ src/app/game-of-life/page.tsx
@@ -1,7 +1,14 @@
 import Board from "./Board";
 
 export default async function GameOfLife() {
-  const board = new Array(10).fill(new Array(20).fill(false));
+  let board = new Array(10).fill([]);
+  board = board.map((line) => new Array(20).fill(false));
+
+  board[0][1] = true;
+  board[1][2] = true;
+  board[2][0] = true;
+  board[2][1] = true;
+  board[2][2] = true;
 
   return <Board board={board} />;
 }
